ae0688a01470e03f5cf47c1f89a66f959d52a587
j2thex <jnamed@gmail.com>
2024-12-28 15:52:33 +0100

    reorg

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 
 # Table of contents
-[lecture0](./lecture0-search.md)
+[lecture0](./week0/lecture0-search.md)
 
-[lecture1](./lecture1-knowledge.md)
+[lecture1](./week1/lecture1-knowledge.md)
--- week0/degrees/degrees.py
+++ week0/degrees/degrees.py
@@ -107,7 +107,7 @@ def shortest_path(source, target):
     while True:
         # If nothing left in frontier, then no path
         if frontier.empty():
-            raise Exception("no solution")
+            return None
 
         # Choose a node from the frontier
         node = frontier.remove()
